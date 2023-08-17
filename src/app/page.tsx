b498b1847136d164027159233afa9aae4c1ad094
kimwooyoung <whoyoung90@naver.com>
2023-08-18 00:09:16 +0900

    fix: 230817 페이지 레이아웃 만들기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Main
-    </main>
-  );
+  return <section className="bg-gray-500">Main</section>;
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,4 @@
+export default function Footer() {
+  // 싱글 따옴표(')를 쓸 경우, HTML 엔티티를 사용하기 싫다면
+  return <p>{"Don't forget to CODE your DREAM | ALl Right Reserved."}</p>;
+}
