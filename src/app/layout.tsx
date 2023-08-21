f50f04d41e0b557b26fd0d63b20e9a3c7d89d1d4
kimwooyoung <whoyoung90@naver.com>
2023-08-22 00:08:08 +0900

    study: 230822 프로필 만들기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import Header from "@/components/Header";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
+import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 const sans = Open_Sans({ subsets: ["latin"] });
@@ -18,9 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={sans.className}>
-      <body>
+      <body className="flex flex-col w-full max-w-screen-2xl mx-auto">
         <Header />
-        <main>{children}</main>
+        <main className="grow">{children}</main>
         <Footer />
       </body>
     </html>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,8 @@
 export default function Footer() {
   // 싱글 따옴표(')를 쓸 경우, HTML 엔티티를 사용하기 싫다면
-  return <p>{"Don't forget to CODE your DREAM | ALl Right Reserved."}</p>;
+  return (
+    <p className="bg-slate-900 text-white py-2 text-sm text-center">
+      {"Don't forget to CODE your DREAM | ALl Right Reserved."}
+    </p>
+  );
 }
