c671126f86dcc6488e39c4815ea9de70ab66c35a
kimwooyoung <whoyoung90@naver.com>
2023-08-29 00:17:47 +0900

    study: 230828 피쳐된 포스트 보여주기-스타일링

--- src/components/PostsGrid.tsx
+++ src/components/PostsGrid.tsx
@@ -8,7 +8,7 @@ type Props = {
 // export default function PostsGrid(props: Props) {
 export default function PostsGrid({ posts }: Props) {
   return (
-    <ul>
+    <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {posts.map((post) => (
         <li key={post.path}>
           <PostCard post={post} />
