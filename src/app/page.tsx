1df70da0a9a4e2083bdb8b0b5a39c27ecbfaec04
kimwooyoung <whoyoung90@naver.com>
2023-08-30 23:20:53 +0900

    fix: 캐러셀 포스트 보여주기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import FeaturedPost from "@/components/FeaturedPosts";
+import CarouselPosts from "@/components/CarouselPosts";
 
 export default function HomePage() {
   return (
@@ -7,6 +8,8 @@ export default function HomePage() {
       <Hero />
       {/* @ts-expect-error Server Component */}
       <FeaturedPost />
+      {/* @ts-expect-error Server Component */}
+      <CarouselPosts />
     </>
   );
 }
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -7,7 +7,7 @@ export default function PostCard({
   post: { title, description, date, category, path },
 }: Props) {
   return (
-    <article className="rounded-md overflow-hidden shadow-lg">
+    <article className="rounded-md overflow-hidden shadow-md hover:shadow-xl">
       <Link href={`/posts/${path}`}>
         <Image
           className="w-full"
@@ -17,7 +17,7 @@ export default function PostCard({
           height={200}
         />
         <div className="flex flex-col items-center p-4">
-          <time className="self-end">{date.toString()}</time>
+          <time className="self-end text-gray-700">{date.toString()}</time>
           <h3 className="text-lg font-bold">{title}</h3>
           <p className="w-full truncate text-center">{description}</p>
           <span className="text-sm rounded-lg bg-green-100 px-2 my-2">
