9dfa84fb1a0146bf5c7beba2775c548fa31dd22a
kimwooyoung <whoyoung90@naver.com>
2023-09-06 00:10:45 +0900

    study: 230905 마크다운 뷰어-상세정보

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -2,6 +2,7 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 export default function MarkdownViewer({ content }: { content: string }) {
+  // tailwind 사용시 적용되는 css reset을 "prose"옵션으로 막을 수 있다. (기본 스타일링 적용)
   return (
     <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
       {content}
