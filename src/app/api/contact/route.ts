22a12a86aab60075abc6548885e56b6561e8be0e
kimwooyoung <whoyoung90@naver.com>
2023-10-02 18:51:46 +0900

    study: 231002 이메일 보내기 - 서버 라우트

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,4 +1,5 @@
 import * as yup from "yup";
+import { sendEmail } from "@/API/email";
 
 // yup: 데이터 유효성 검사 라이브러리
 const bodySchema = yup.object().shape({
@@ -13,10 +14,29 @@ const bodySchema = yup.object().shape({
  * 특정 경로에 GET, POST, PUT, DELETE과 같은 요청이 오면 그때 함수가 실행되도록!
  */
 export async function POST(req: Request) {
-  if (!bodySchema.isValidSync(req.body)) {
-    return new Response("유효하지 않은 포맷", { status: 400 });
+  const body = await req.json(); // *[error] ReadableStream
+
+  if (!bodySchema.isValidSync(body)) {
+    return new Response(JSON.stringify({ message: "메일 전송에 실패함!" }), {
+      status: 400,
+    });
   }
 
   // [2] 노드 메일러를 이용하여 메일 전송
-  const { from, subject, message } = req.body;
+  return sendEmail(body) //
+    .then(
+      () =>
+        new Response(JSON.stringify({ message: "메일을 성공적으로 보냈음" }), {
+          status: 200,
+        })
+    )
+    .catch((error) => {
+      console.error(error);
+      new Response(JSON.stringify({ message: "메일 전송에 실패함!" }), {
+        status: 500,
+      });
+    });
 }
+
+// *: API Route에서 사용하는 Request는 Node에서 사용하는 Request와 동일하므로 body는 ReadableStream이다.
+// 그러므로 JSON으로 변환해야 한다!
